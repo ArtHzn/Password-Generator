d503c3fcf0b879246f975115a0a7a042b8c75de2
ArtHzn <arturmaj@hotmail.com>
2023-03-27 18:55:13 +0200

    Updated password generator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,6 +1,8 @@
 import string
 import random
 import time
+import pyperclip
+
 
 keepUsing = True
 length = 0
@@ -43,6 +45,27 @@ while keepUsing == True:
 
     #printing password
     print("Your safe password: ", password )
+    
+    #loop asking user if they want to copy password to clipboard
+    while True:
+        try:
+            copyPassword = str(input("Do You want this password to be copied to clipboard? Press Y or N: ")).lower()
+            if copyPassword in ['y', 'n']:   
+                if copyPassword == "y":
+                    #copy password to clipboard
+                    pyperclip.copy(password)
+                    print("Password copied to clipboard!")
+                    break
+                if copyPassword == "n" :
+                    break
+            else:
+                ValueError()
+                print("Enter Y or N")
+        except:
+            ValueError()
+            print("Enter Y or N")
+    
+    #loop asking user if they want to generate another password
     while True:
         try:  
             anotherOne = str(input("Do You want to generate another password? Press Y to continue or N to exit: ")).lower()
@@ -59,7 +82,8 @@ while keepUsing == True:
         except:
             ValueError()
             print("Enter Y or N")
-            
+
+#end message            
 print("Thank You for using my password generator. App is closing...")
 time.sleep(2)
 
